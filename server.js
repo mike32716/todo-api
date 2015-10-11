4118be72b47ecea1975859066fc10fa338196db7
Michael Hansen <mike32716@gmail.com>
2015-10-11 16:41:58 -0400

    add POST for todos

--- server.js
+++ server.js
@@ -1,25 +1,17 @@
 //SERVER.JS
 
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000;  //if running on Haroku
 
 // A todo is the model = 1 object.  A set of todos is
 // called a collection.
-var todos =[
-  {
-    id: 1,
-    description: 'Meet mom for lunch',
-    completed: false
-},  {
-    id: 2,
-    description: 'Go to market',
-    completed: false
-},  {
-    id: 3,
-    description: 'Clean up the office',
-    completed: true
-}];
+var todos =[];
+var todoNextId = 1;  //not secure.  Use for counting.
+
+app.use(bodyParser.json());  //access vis request.body
+
 
 //*****************************************************
 app.get('/', function (req, res) {
@@ -53,6 +45,28 @@ app.get('/todos/:id',  function (req, res){
   //res.send('Asking for todo with id of ' + req.params.id);
 });
 
+//*******************************************************
+//POST /todos
+
+app.post('/todos', function(req, res){
+    var body = req.body;  //get the JSON object inputed
+
+    todo = body;
+    todo.id = todoNextId;
+    todos.push(todo);
+    todoNextId = todoNextId + 1;
+    // or you can do it this way like he did.
+    //  body.id = todoNextId++;  //assigns id THEN incriments
+    //  todos.push(body);
+
+
+    res.json(body);
+    //  console.log('description: ' + body.description);
+});
+
+
+
+
 
 //*********************************************************
 app.listen(PORT, function(){
@@ -67,3 +81,25 @@ app.listen(PORT, function(){
 
 
 //
+/*  Test DATA
+var todos =[
+  {
+    id: 1,
+    description: 'Meet mom for lunch',
+    completed: false
+},  {
+    id: 2,
+    description: 'Go to market',
+    completed: false
+},  {
+    id: 3,
+    description: 'Clean up the office and GET to work!',
+    completed: true
+}];
+
+some JSON
+{
+    "description": "Go to Publix",
+    "completed": false
+}
+*/
