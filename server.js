82ea4daca342cb4f1dc98f73c39a89b5cbe577b9
Michael Hansen <mike32716@gmail.com>
2015-10-11 13:32:44 -0400

    Add /todos and /todos/:id

--- server.js
+++ server.js
@@ -2,15 +2,68 @@
 
 var express = require('express');
 var app = express();
-
 var PORT = process.env.PORT || 3000;  //if running on Haroku
 
+// A todo is the model = 1 object.  A set of todos is
+// called a collection.
+var todos =[
+  {
+    id: 1,
+    description: 'Meet mom for lunch',
+    completed: false
+},  {
+    id: 2,
+    description: 'Go to market',
+    completed: false
+},  {
+    id: 3,
+    description: 'Clean up the office',
+    completed: true
+}];
+
+//*****************************************************
 app.get('/', function (req, res) {
   res.send('Todo API Root');
 })
 
+
+//******************************************************
+// GET /todo
+app.get('/todos', function (req, res){
+  res.json(todos);   // This will convert array into json
+                     // ans send back the api
+});
+
+
+//GET /todos/:id
+app.get('/todos/:id',  function (req, res){
+    var todoId = parseInt(req.params.id,10);  //parseInt sets string to integet in base 10
+                                              //subtracting zero alos worked as it forced it to be an int.
+    var matchedTodo;
+
+    todos.forEach(function(todoList) {  //pass in object toodoList in the array todos
+        if (todoId === todoList.id){matchedTodo = todoList;}
+    });
+
+    if(matchedTodo){res.json(matchedTodo);}
+
+    else {res.status(404).send('Sorry, there is no match for id: ' + req.params.id);
+          }   //if NO match
+
+  //res.send('Asking for todo with id of ' + req.params.id);
+});
+
+
+//*********************************************************
 app.listen(PORT, function(){
   console.log('Express listening on port ' + PORT + '!');
+});
 
-  
-})
+
+
+
+
+
+
+
+//
