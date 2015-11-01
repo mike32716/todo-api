265d3cee047fdbe2e46ac6732ccdac12c3718da4
Michael Hansen <mike32716@gmail.com>
2015-11-01 13:51:41 -0500

    added email case validation tolowercase

--- models/user.js
+++ models/user.js
@@ -7,19 +7,33 @@ module.exports = function(sequelize, DataTypes) {
 
     return sequelize.define('user', {
 
-        email: {
-            type: DataTypes.STRING,   //use DataTypes when being called by sequelize.import
-            allowNull: false,
-            unique: true, //no other record with same value in database
-            validate: {	isEmail: true }
+            email: {
+                type: DataTypes.STRING,   //use DataTypes when being called by sequelize.import
+                allowNull: false,
+                unique: true, //no other record with same value in database
+                validate: {	isEmail: true }
+            },
+
+            password: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {len: [7, 100]}
+             }
+
         },
 
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {len: [7, 100]}
-         }
+        {
+            hooks: {
+                beforeValidate: function (user, options) {
+                    if(typeof user.email === 'string'){
+                        user.email = user.email.toLowerCase();
+                    }
 
-    });
+                }
+
+            }
+        });
+
+
 };
 
